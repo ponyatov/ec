2868557af8645b243c248aa5b308dcbc240d4fea
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-19 06:32:28 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/filegen.ml
+++ lib/filegen.ml
@@ -1,2 +1,17 @@
 (** generate project structure *)
 
+let mk () =
+  let mf = open_out "Makefile" in
+  mf |> ignore mk
+
+let giti () =
+  let giti = open_out ".gitignore" in
+  giti |> ignore giti
+
+let cf () =
+  let cf = open_out ".clang-format" in
+  cf |> ignore
+
+let ofmt () =
+  let ofmt = open_out ".ocamlformat" in
+  ofmt |> ignore
