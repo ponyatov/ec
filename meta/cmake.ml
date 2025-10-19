931ff134086b486bb469c0c86ed5808e41f9aa2e
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-20 00:45:33 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- meta/cmake.ml
+++ meta/cmake.ml
@@ -4,6 +4,9 @@ let lists () =
   let f = open_out "CMakeLists.txt" in
   close_out f
 
-let lists () =
+let presets () =
   let f = open_out "CMakePresets.json" in
   close_out f
+
+  Sys.mkdir "cmake" 0o755 ;
+ open_out "cmake/.gitignore" |> close_out
