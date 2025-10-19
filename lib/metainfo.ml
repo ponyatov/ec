9470c0c47578b821d48ff9df80e7fd22710ba78d
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-20 00:11:18 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/metainfo.ml
+++ lib/metainfo.ml
@@ -7,7 +7,14 @@ let author = "Dmitry Ponyatov"
 let email = "dponyatov@gmail.com"
 let year = 2025
 let license = "MIT"
-let github = "https://github.com/ponyatov/${APP}"
+let github = "https://github.com/ponyatov/"
 
-let readme () = 
-  print_endline ("# `" ^ app ^ "` " ^ version ^ "\n## " ^ title ^ "\n\n")
+let readme () =
+  let readme = open_out "README.md" in
+  output_string readme
+    ("# `" ^ app ^ "` " ^ version ^ "\n## " ^ title ^ "\n\n" ^ "(c) " ^ author
+   ^ " <<" ^ email ^ ">> " ^ string_of_int year ^ " " ^ license ^ "\n\ngithub: "
+   ^ github ^ app ^ "\n");
+  close_out readme
+
+(* readme () *)
