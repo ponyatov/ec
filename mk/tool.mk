7570445b1c1e506818bdac42d310b398252204fe
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-20 10:29:15 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ mk/tool.mk
@@ -0,0 +1,33 @@
+CURL   = curl -L -o
+CF     = clang-format -style=file -i
+GITREF = git clone -o gh --depth 1
+#
+PY     = python3
+PIP    = pip3
+PEP    = autopep8 --ignore $(PEPS) -i
+#
+PY     = $(BIN)/python3
+PIP    = $(BIN)/pip3
+PEP    = $(BIN)/autopep8 --ignore $(PEPS) -i
+#
+RUSTUP = $(CAR)/bin/rustucp p
+CARGO  = $(CAR)/bin/cargo
+#
+OPAM   = /usr/local/bin/opam
+OCAMLC = $(ODIR)/ocamlc
+UTOP   = $(ODIR)/utop
+DUNE   = $(ODIR)/dune
+OFMT   = $(ODIR)/ocamlformat
+OLSP   = $(ODIR)/ocamllsp
+MENHIR = $(ODIR)/menhir
+#
+NPM    = /usr/bin/npm
+TSC    = $(HOME)/.npm/bin/tsc
+DENO   = $(HOME)/.npm/bin/deno
+#
+GO     = /usr/local/go/bin/go
+GOPLS  = $(GOPATH)/bin/gopls
+YO     = $(HOME)/.npm/bin/yo
+#
+QUCS   = /usr/bin/qucs-s
+SPICE  = /usr/bin/ngspice
