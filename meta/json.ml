b82c8dec370fb2867a1eb3d5a681c126d93ee229
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-20 10:55:48 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- meta/json.ml
+++ meta/json.ml
@@ -3,3 +3,14 @@
     - from: lib/sample.json
     - tutorial:
       https://dev.realworldocaml.org/parsing-with-ocamllex-and-menhir.html *)
+
+type token =          (**)
+  | NULL              (* null   *)
+  | TRUE | FALSE      (* bool:  true|fase *)
+  | STRING of string  (* str    *)
+  | INT of int        (* int    *)
+  | FLOAT of float    (* float  *)
+  | LQ | RQ           (* []     *)
+  | LC | RC           (* {}     *)
+  | COMMA | COLON     (* , :    *)
+  | EOF               (* end-of-file *)
