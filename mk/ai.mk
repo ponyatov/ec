e8861c73b2d465b0d0023c7d1cee0b8eb287d337
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-23 16:59:17 +0400

    .

--- mk/ai.mk
+++ mk/ai.mk
@@ -1,4 +1,5 @@
 .PHONY: ai tmp/$(APP).ai.md
 ai: tmp/$(APP).ai.md
 tmp/$(APP).ai.md:
-	cat README.md $(HOME)/metadoc/E/*.md $(HOME)/metadoc/OCaml/*.md $(M) > $@ ; touch $@
+	cat $(HOME)/metadoc/ec/*.md > $@ ; touch $@
+# 	cat README.md $(HOME)/metadoc/E/*.md $(HOME)/metadoc/OCaml/*.md $(M) > $@ ; touch $@
